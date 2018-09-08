f809ce73a678813d61ce5338f37cd2a190b89bdf
Robert Edström <robert.edstrom@gmail.com>
2018-09-08 15:29:04 +0200

    Add SearchResults

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,22 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Grid, Typography } from '@material-ui/core'
 import { withStyles } from '@material-ui/core/styles'
 
 import Social from '../Social/Social'
 import Searchbar from '../Searchbar'
+import SearchResult from '../SearchResult'
 import styles from './Layout.styles'
 
-const Layout = ({ name, classes }) => (
-  <Grid className={classes.root}>
-    <span className={classes.shape1} />
-    <span className={classes.shape2} />
-    <span className={classes.shape3} />
-    <Grid container direction="column" className={classes.section}>
-      <Searchbar onSearch={(value) => {console.log('SEEEEARCH', value)}} />
-    </Grid>
-    <span className={classes.ornament} />
-    <Social className={classes.social} />
-  </Grid>
-)
+class Layout extends Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+        searchQuery: null
+      };
+  }
+  render() {
+    console.log('props', this.props);
+    console.log('classes', this.classes)
+    const { name, classes } = this.props;
+    return (
+      <Grid className={classes.root}>
+      <span className={classes.shape1} />
+      <span className={classes.shape2} />
+      <span className={classes.shape3} />
+        <Grid container direction="column" className={classes.section}>
+          <Searchbar onSearch={(searchQuery) => {console.log('SEEEEARCH', searchQuery); this.setState({ searchQuery })}} />
+          <SearchResult query={this.state.searchQuery}/>
+        </Grid>
+        <span className={classes.ornament} />
+        <Social className={classes.social} />
+      </Grid>
+    )
+  }
+}
 
 export default withStyles(styles)(Layout)
---
+++ src/components/SearchResult/SearchResult.js
@@ -0,0 +1,66 @@
+import React, { Component, Fragment } from 'react';
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
+
+const ETH_ADDRESS_PATTERN = /^0x[a-fA-F0-9]{40}$/
+
+const GET_ENS_NAME_QUERY = gql`
+  query ensName($name: String) {
+    ensName(name: $name) {
+      name
+      ownerHistory {
+        address
+      }
+      resolverHistory {
+        address
+      }
+    }
+  }
+`
+
+const GET_OWNERSHIPS_QUERY = gql`
+  query ensName($name: String) {
+    ensName(name: $name) {
+      name
+      ownerHistory {
+        address
+      }
+      resolverHistory {
+        address
+      }
+    }
+  }
+`
+const getSearchVariables = (query) =>
+  query.match(ETH_ADDRESS_PATTERN)
+    ? { owner: query }
+    : { name: query }
+
+const getSearchQuery = (query) =>
+  query.match(ETH_ADDRESS_PATTERN)
+    ? GET_OWNERSHIPS_QUERY
+    : GET_ENS_NAME_QUERY
+
+class SearchResult extends Component {
+  render() {
+    const { props } = this;
+    const { query } = props;
+    return (
+      props.query && props.query.length > 0
+        ?
+          <Query query={getSearchQuery(query)} variables={getSearchVariables(query)}>
+            {({ loading, error, data }) => {
+              if (loading) return 'Loading...'
+              if (error) return `Error! ${error.message}`
+
+              return <Fragment>
+                {JSON.stringify(data)}
+              </Fragment>
+            }}
+          </Query>
+        : <div>search plz</div>
+    )
+  }
+}
+
+export default SearchResult
