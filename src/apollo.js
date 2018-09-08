e72fa5b87b8ed648493ca93c167f704259a22eea
Jeff Lau <jeff-lau@live.com>
2018-09-08 02:19:56 +0200

    Basic schema and mock data

--- src/apollo.js
+++ src/apollo.js
@@ -1,14 +1,25 @@
 import ApolloClient from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { SchemaLink } from 'apollo-link-schema'
 import fetch from 'unfetch'
+import typeDefs from './types'
+import resolvers from './resolvers'
 
-const endpoints = {
-  simple: 'https://us1.prisma.sh/nevena-djaja/hello-hacker/dev',
-  ws: 'wss://us1.prisma.sh/nevena-djaja/hello-hacker/dev'
-}
+import { makeExecutableSchema } from 'graphql-tools'
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+})
+
+// const endpoints = {
+//   simple: 'https://us1.prisma.sh/nevena-djaja/hello-hacker/dev',
+//   ws: 'wss://us1.prisma.sh/nevena-djaja/hello-hacker/dev'
+// }
 
 export const apolloClient = new ApolloClient({
-  link: new HttpLink({ uri: endpoints.simple, fetch: fetch }),
+  link: new SchemaLink({ schema }),
+  //link: new HttpLink({ uri: endpoints.simple, fetch: fetch }),
   cache: new InMemoryCache()
 })
