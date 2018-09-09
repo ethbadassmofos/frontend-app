6aba71b0a40181e8967226600ea916c7f65d80d6
Jeff Lau <jeff-lau@live.com>
2018-09-09 04:23:42 +0200

    Added in functions to decryptHashes

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -3,16 +3,55 @@ import React from 'react'
 import styled from 'styled-components'
 
 import ChainValue from '../ChainValue'
+import decryptHashes from '../../preimage'
+import R from 'ramda'
 
 class Table extends React.Component {
   state = {
-    limit: 25
+    limit: 25,
+    step: 25,
+    loadingHashes: false,
+    names: {}
   }
+  // async getHashes() {
+  //   const hashes = data.slice(this.state.limit - step, this.state.limit)
+  //   const names = await decryptHashes(hashes).map(node => node.labelHash)
+  //   const nameHashMap = R.zipObj(hashes, names)
+
+  //   this.setState({
+  //     names: {
+  //       ...this.state.names,
+  //       ...nameHashMap
+  //     },
+  //     hashesLoaded: this.state.limit
+  //   })
+  // }
+  // async componentDidMount() {
+  //   this.getHashes()
+  // }
+
+  // async componentDidUpdate() {
+  //   if (this.state.hashedLoaded === this.state.limit) {
+  //     return
+  //   }
+
+  //   this.getHashes()
+  // }
+
   render() {
     const { data, className, paging = false } = this.props
     let slicedData
     if (paging) {
-      slicedData = data.slice(0, this.state.limit)
+      slicedData = data.slice(0, this.state.limit).map(item => {
+        if (this.state.names[item.labelHash]) {
+          return {
+            ...item,
+            name: this.state.names[item.labelHash]
+          }
+        }
+
+        return item
+      })
     }
     const labels = Object.keys(data[0])
 
--- src/utils/table.js
+++ src/utils/table.js
@@ -4,11 +4,11 @@ export const processNodeEventData = history => {
       .map(value => {
         switch (value[0]) {
           case 'node': {
-            value = [ value[0], value[1].nameHash ]
+            value = [value[0], value[1].nameHash]
             break
           }
           case 'actor': {
-            value = [ value[0], value[1].address, { type: 'address' } ]
+            value = [value[0], value[1].address, { type: 'address' }]
             break
           }
           default: {
@@ -29,12 +29,14 @@ export const processNodeEventData = history => {
   return massagedData
 }
 
-export const processNodeData = nodes => (
-  nodes.map(node => (
+export const processNodeData = nodes =>
+  nodes.map(node =>
     Object.keys(node).reduce((ret, key) => {
+      if (key === '__typename') {
+        return ret
+      }
       ret[key] = { value: node[key], options: {} }
 
       return ret
     }, {})
-  ))
-)
+  )
